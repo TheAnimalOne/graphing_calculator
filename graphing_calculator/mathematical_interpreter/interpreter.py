dd447a160869e2b69f5049cff7583cf4d467707a
houru <houruxuan01@gmail.com>
2025-07-06 00:33:43 +1000

    (feat) reformatting, moved all parser and lexer files into one. created func for interpreting mathematical expressions and added combined tests into 1 file.

--- graphing_calculator/mathematical_interpreter/interpreter.py
+++ graphing_calculator/mathematical_interpreter/interpreter.py
@@ -1,6 +1,16 @@
 import re
 
-from graphing_calculator.functional_tree.nodes import OPERATOR_SYMBOL
+from graphing_calculator.functional_tree.nodes import (
+    NodeType,
+    OPERATOR_SYMBOL,
+    ConstantNode,
+    VariableNode,
+    AdditionNode,
+    SubtractionNode,
+    MultiplicationNode,
+    DivisionNode,
+    ExponentialNode,
+)
 
 
 NUMBER = r"-?\d*\.{0,1}\d+"
@@ -13,6 +23,13 @@ PRECEDENCE = {
     OPERATOR_SYMBOL.ADD.value: (0, "left"),
     OPERATOR_SYMBOL.SUB.value: (0, "left"),
 }
+NODE_BY_OPERATOR = {
+    OPERATOR_SYMBOL.EXP.value: ExponentialNode,
+    OPERATOR_SYMBOL.MULT.value: MultiplicationNode,
+    OPERATOR_SYMBOL.DIV.value: DivisionNode,
+    OPERATOR_SYMBOL.ADD.value: AdditionNode,
+    OPERATOR_SYMBOL.SUB.value: SubtractionNode,
+}
 
 
 def tokeniser(text: str) -> list[str]:
@@ -22,6 +39,7 @@ def tokeniser(text: str) -> list[str]:
 
 
 def check_operator_precedence(op1: str, op2: str) -> bool:
+    """Check operator precedence, taking into account the associativity"""
     op1_precedence, op1_associativity = PRECEDENCE[op1]
     op2_precedence, _ = PRECEDENCE[op2]
     if op2_precedence > op1_precedence:
@@ -50,7 +68,7 @@ def shunting_yard_lexer(tokens: list[str]) -> list[str]:
                     raise ValueError(f"Mismatched parenthesis, left parenthesis not found in operator stack")
                 output.append(operator_stack.pop())
             if operator_stack[-1] != "(":
-                raise ValueError(f"Mismatched parenthesis, left parenthesis not found in operator stack")\
+                raise ValueError(f"Mismatched parenthesis, left parenthesis not found in operator stack")
             # remove l paren
             operator_stack.pop()
         else:
@@ -60,3 +78,31 @@ def shunting_yard_lexer(tokens: list[str]) -> list[str]:
             raise ValueError(f"Mismatched parenthesis found on operator stack: {operator_stack}")
         output.append(operator_stack.pop())
     return output
+
+
+def parse_rpn_to_functional_tree(output_rpn: list[str]) -> NodeType:
+    """Parses an expression in RPN form into a Node Tree"""
+    output = []
+    for token in output_rpn:
+        if re.match(NUMBER, token):
+            node = ConstantNode(float(token))
+        elif re.match(VARIABLE, token):
+            node =  VariableNode(token)
+        elif re.match(SYMBOLS, token):
+            r_node = output.pop()
+            l_node = output.pop()
+            node = NODE_BY_OPERATOR[token](l_node, r_node)
+        else:
+            raise ValueError(f"Failure to parse {token=}")
+        output.append(node)
+    if len(output) == 1:
+        return output[0]
+    raise ValueError(f"Parse failure for {output_rpn=}, final stack position is {output=}")
+
+
+def interpret_text_to_tree(mathematical_expression: str) -> NodeType:
+    """Interprets a mathematical expression as a Node Tree"""
+    tokens = tokeniser(mathematical_expression)
+    rpn = shunting_yard_lexer(tokens)
+    tree = parse_rpn_to_functional_tree(rpn)
+    return tree
---
+++ tests/test_mathematical_interpreter/test_interpreter.py
@@ -0,0 +1,80 @@
+import pytest
+
+from graphing_calculator.mathematical_interpreter.interpreter import (
+    tokeniser,
+    shunting_yard_lexer,
+    parse_rpn_to_functional_tree,
+    interpret_text_to_tree,
+)
+
+
+TEST_NAMES_LEXER = "input_text, expected_result"
+TEST_TOKENISER = [
+    "21 + 3423 + 34324.342412 + 33423412.145123432134 + x",
+    "3 ^ 1233.2 * 4753 + 123 + -3213.2",
+    "adf2 + dsf31 - sA3asd / 23123.2131 * 23123.2",
+    "( ( 20 - 10 ) * ( 30 - 20 ) / 10 + 10 ) * 2"
+]
+TEST_LEXER = {
+    "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3": "3 4 2 * 1 5 - 2 3 ^ ^ / +".split(" "),
+    "( ( 20 - 10 ) * ( 30 - 20 ) / 10 + 10 ) * 2": "20 10 - 30 20 - * 10 / 10 + 2 *".split(" "),
+}
+TEST_LEXER_FAIL = [
+    "( ( ( ( 3 ) ) ) ) )",
+    "( ( ( ( 3 ) ) ) ) (",
+    " + + + + ) ) ) ) (",
+    "!@# $@# %^",
+]
+
+
+TEST_NAMES_PARSER = "rpn, expected_result"
+TEST_PARSER = {
+    "x 3 +": 6,
+    "3 4 2 * 1 5 - 2 3 ^ ^ / +": 3.0001220703125,
+    "x 4 2 * 1 5 - 2 x ^ ^ / +": 3.0001220703125, # check works with variable
+    "20 10 - 30 20 - * 10 / 10 + 2 *": 40,
+    "20 10 - 30 20 - * 10 / 10 + x 1 - *": 40, # check works with variable
+}
+
+
+TEST_NAMES_INTERPRET = "math_expression, expected_result"
+TEST_INTERPRETER = [
+    "x^3 + 3*x^2 + 3*x + 1",
+    "32+2+17/31/3*2134",
+    "x ^ x",
+    "x * x - x ^ 2",
+    "x*x-x^2",
+    "x*(x-x)^2",
+    "x^(3+3)*x^(2+3)*(x+1)",
+]
+
+
+@pytest.mark.parametrize(TEST_NAMES_LEXER, [(v, v.split(" ")) for v in TEST_TOKENISER])
+def test_tokeniser(input_text: str, expected_result: list[str]):
+    assert tokeniser(input_text) == expected_result
+
+
+@pytest.mark.parametrize(TEST_NAMES_LEXER, [(k.split(" "), v) for k, v in TEST_LEXER.items()])
+def test_shunting_yard_lexer(input_text: list[str], expected_result: list[str]):
+    assert shunting_yard_lexer(input_text) == expected_result
+
+
+@pytest.mark.parametrize("input_text", [txt.split(" ") for txt in TEST_LEXER_FAIL])
+def test_shunting_yard_lexer_fail(input_text: list[str]):
+    with pytest.raises(Exception):
+        shunting_yard_lexer(input_text)
+
+
+@pytest.mark.parametrize(TEST_NAMES_PARSER, TEST_PARSER.items())
+def test_parse_rpn_to_functional_tree(rpn: str, expected_result: float):
+    tree =  parse_rpn_to_functional_tree(rpn.split(" "))
+    assert tree.evaluate({"x": 3}) == expected_result
+
+
+@pytest.mark.parametrize(
+    TEST_NAMES_INTERPRET,
+    [(txt, eval(txt.replace("x", "3").replace("^", "**"))) for txt in TEST_INTERPRETER]
+)
+def test_interpret_text_to_tree(math_expression: str, expected_result: float):
+    tree = interpret_text_to_tree(math_expression)
+    assert tree.evaluate({"x": 3}) == expected_result
