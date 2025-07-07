5dfb1903f67e7d8ff0ee66a1d4a606bd30f1ffae
houru <houruxuan01@gmail.com>
2025-07-07 23:25:54 +1000

    (feat) finish calculate tab ui

--- tests/test_functional_tree/test_function_node.py
+++ tests/test_functional_tree/test_function_node.py
@@ -48,14 +48,14 @@ TEST_NAMES_FUNC = ("name", "tree", "expected_result")
 
 TEST_FUNC = {
     ("f", X_AND_Y): {
-        "grad": "f' () = 1",
+        "grad": "f_x () = 1",
         "eval": 20,
         "part_eval": "f_1 (y) = 11 + (y + 8)",
         "str": "f (x, y) = (x + 8) + (y + 8)",
         "repr": f"<FunctionNode: f -> ['x', 'y']> = {repr(X_AND_Y)}"
     },
     ("g", X_AND_Y_AND_Z_1): {
-        "grad": "g' (z) = 0 + 1 * z",
+        "grad": "g_x (z) = 0 + 1 * z",
         "eval": 40,
         "part_eval": "g_1 (y, z) = z * (11 + (y + 8))",
         "str": "g (x, y, z) = z * ((x + 8) + (y + 8))",
